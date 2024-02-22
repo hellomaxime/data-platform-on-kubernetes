4760d83816ced736b409cf4d527d19bc67d4979b
hellomaxime <86252821+hellomaxime@users.noreply.github.com>
2024-02-22 21:18:23 +0100

    add superset

--- start.sh
+++ start.sh
@@ -2,12 +2,16 @@
 
 echo "Starting data plaform..."
 
+# add repositories
 helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo add superset https://apache.github.io/superset
 
 # install tools
 kubectl create namespace mysql
-helm install mysql bitnami/mysql --namespace mysql
-kubectl apply -f templates/phpmyadmin.yaml
+kubectl apply -f kube_files/mysql.yaml
+kubectl apply -f kube_files/phpmyadmin.yaml
 
 kubectl create namespace spark
 helm install spark bitnami/spark --namespace spark
+
+helm upgrade --install --values values/superset-values.yaml superset superset/superset
--- stop.sh
+++ stop.sh
@@ -3,9 +3,11 @@
 echo "Stopping data platform..."
 
 # uninstall tools
-helm uninstall mysql -n mysql
-kubectl delete -f templates/phpmyadmin.yaml
+kubectl delete -f kube_files/mysql.yaml
+kubectl delete -f kube_files/phpmyadmin.yaml
 kubectl delete namespace mysql
 
 helm uninstall spark -n spark
 kubectl delete namespace spark
+
+helm uninstall superset
