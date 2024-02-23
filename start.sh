f28b17ac65b4969564e26aea1c11f6ba11f0b0e0
hellomaxime <86252821+hellomaxime@users.noreply.github.com>
2024-02-23 10:15:02 +0100

    fix jupyterhub

--- start.sh
+++ start.sh
@@ -5,6 +5,8 @@ echo "Starting data plaform..."
 # add repositories
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add superset https://apache.github.io/superset
+helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
+helm repo update
 
 # install tools
 kubectl create namespace mysql
@@ -16,4 +18,4 @@ helm install spark bitnami/spark --namespace spark
 
 helm upgrade --install --values values/superset-values.yaml superset superset/superset
 
-helm upgrade --install --values values/jupyterhub-values.yaml jupyterhub bitnami/jupyterhub
+helm upgrade --install --values values/jupyterhub-values.yaml jupyterhub jupyterhub/jupyterhub
